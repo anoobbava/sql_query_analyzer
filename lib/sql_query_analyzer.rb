4c465d1cb8c20a99914d38021a7ca57f6d1a15ff
Anoob Bava <anoob.bava@gmail.com>
2025-04-28 18:09:39 +0530

    Added initial commit

---
+++ lib/sql_query_analyzer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sql_query_analyzer/version"
+
+module SqlQueryAnalyzer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sql_query_analyzer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SqlQueryAnalyzer
+  VERSION = "0.1.0"
+end
